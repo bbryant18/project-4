de0ced5cc0e309b95e2625e61a71a512fb85cf9e
bbryant18 <36276047+bbryant18@users.noreply.github.com>
2018-03-22 12:20:27 -0500

    Create isitprime.py

---
+++ isitprime.py
@@ -0,0 +1,26 @@
+# This code has a few issues when not prime numbers are inputted, but that's the only problem I've encountered.
+
+n = input()
+# make sure n is a positive integer
+n = abs(int(n))
+
+# 0 and 1 are not primes
+if n < 2:
+    print("Not Prime")
+
+  # 2 is the only even prime number
+if n == 2: 
+    print("Prime")    
+  
+    # all other even numbers are not primes
+if not n & 1: 
+    print("Not Prime")
+
+    # range starts with 3 and only needs to go up 
+    # the square root of n for all odd numbers
+for x in range(3, int(n**0.5) + 1, 2):
+    if n % x == 0:
+      print("Not Prime")
+    else:
+      print("Prime")
+    break
